ad851c17f90ee2659eb0d067c29bf8d1f247357e
hamzah2304 <hamzah2304@gmail.com>
2023-02-18 23:14:56 +0000

    initial commit

---
+++ contracts/DynamicSvgNft.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+error ERC721Metadata__URI_QueryFor_NonExistentToken();
+
+contract DynamicSvgNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_lowImageURI;
+    string private s_highImageURI;
+
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) private s_tokenIdToHighValues;
+
+    event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+        s_lowImageURI = svgToImageURI(lowSvg);
+        s_highImageURI = svgToImageURI(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValues[s_tokenCounter] = highValue;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+        emit CreatedNFT(s_tokenCounter, highValue);
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        // example:
+        // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
+        // would return ""
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) {
+            revert ERC721Metadata__URI_QueryFor_NonExistentToken();
+        }
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = s_lowImageURI;
+        if (price >= s_tokenIdToHighValues[tokenId]) {
+            imageURI = s_highImageURI;
+        }
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+}
---
+++ deploy/03-deploy-dynamic-svg-nft.js
@@ -0,0 +1,39 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+const fs = require("fs")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    if (chainId == 31337) {
+        // Find ETH/USD price feed
+        const EthUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = EthUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
+    }
+
+    const lowSVG = fs.readFileSync("./images/dynamicNft/frown.svg", "utf8")
+    const highSVG = fs.readFileSync("./images/dynamicNft/happy.svg", "utf8")
+
+    arguments = [ethUsdPriceFeedAddress, lowSVG, highSVG]
+    const dynamicSvgNft = await deploy("DynamicSvgNft", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    log("----------------------------------------------------")
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(dynamicSvgNft.address, arguments)
+    }
+}
+
+module.exports.tags = ["all", "dynamicsvg", "main"]
